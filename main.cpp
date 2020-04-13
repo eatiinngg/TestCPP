6068fda8fbb7d75d478ecfad66060acc539bd7b5
Eating <s88194@gmail.com>
2020-04-13 21:13:51 +0800

    *constructor prectice

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Algorithm
+{
+    int x,y;
+
+    public:
+    Algorithm(int a, int b);
+    int sum(void)
+    {
+        return x + y;
+    }
+};
+
+Algorithm::Algorithm(int a, int b)
+{
+    x = a;
+    y = b;
+}
+
 
 int main(int argc, const char * argv[])
 {
+    Algorithm fxxk(15,85);
+
+    cout << fxxk.sum() << endl;
+
     printf("Hello\n");
     return 0;
 }
